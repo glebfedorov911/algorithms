3405681ac45b37922bb2f9f1d60c62c2e944d776
glebfedorov911 <fedorov22134@mail.ru>
2023-08-20 15:36:52 +0300

    dasda

--- BFS/BFS.py
+++ BFS/BFS.py
@@ -8,7 +8,9 @@ class Graph:
             self.adjList[src].append(dest)
             self.adjList[dest].append(src)
 
+
 def BFS(graph, v, discovered):
+
     q = deque()
 
     discovered[v] = True
@@ -16,6 +18,7 @@ def BFS(graph, v, discovered):
     q.append(v)
 
     while q:
+
         v = q.popleft()
         print(v, end=" ")
 
@@ -24,14 +27,18 @@ def BFS(graph, v, discovered):
                 discovered[u] = True
                 q.append(u)
 
+
 edges = [
         (1, 2), (1, 3), (1, 4), (2, 5), (2, 6), (5, 9),
         (5, 10), (4, 7), (4, 8), (7, 11), (7, 12)
     ]
 
 n = 15
+
 graph = Graph(edges, n)
+
 discovered = [False]*n
+
 for i in range(n):
     if not discovered[i]:
         BFS(graph, i, discovered)
